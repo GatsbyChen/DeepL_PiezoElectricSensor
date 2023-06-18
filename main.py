144c503f10e1ad657aa32851614c8b61b07814c4
aka3san <tzmtdustnsk.azs@gmail.com>
2023-06-18 21:26:08 +0900

    null

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[8]:
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd 
@@ -18,7 +12,20 @@ import subprocess
 
 #FeatureValueクラスのリストとそれに対応する目的変数のリストから特徴量を取得して、csvファイルで出力する。返り値はDataFlame
 
-def exportCSV(FeatureValueList, targetVarList, name):
+def exportCSV(inputURL, outputURL):
+    #BPDatas配下の各ファイルから特徴量と目的変数を取得する
+    #path_list=glob.glob(r"C:\Users\azlab\OneDrive - 国立大学法人東海国立大学機構\ドキュメント\PZT圧電センサ\BPDatas_tmp" + '/*')
+    path_list = glob.glob(inputURL)
+    fValueList = []
+    targetVarList = []
+    #各データをFV（特徴量）に変換
+    for path in path_list:
+        csv = pd.read_csv(path, encoding="shift-jis")
+        print("↓"+path)
+        fValue = fv.convertToFV(csv, plot=True, fileName=os.path.basename(path).split('.', 1)[0])
+        fValueList.append(fValue)
+        targetVarList.append([csv["SBP"][0], csv["DBP"][0]])
+        
     data = pd.DataFrame()
     idx = 0
     for featureValue in FeatureValueList:   
@@ -42,39 +49,8 @@ def exportCSV(FeatureValueList, targetVarList, name):
         data_tmp["DBP"] = targetVarList[idx][1]
         data = pd.concat([data, data_tmp])
         idx += 1
-    data.to_csv(name, sep=',', encoding='utf-8')
+    data.to_csv(outputURL, sep=',', encoding='utf-8')
     return data
 
-#BPDatas配下の各ファイルから特徴量と目的変数を取得する
 
-#path_list=glob.glob(r"C:\Users\azlab\OneDrive - 国立大学法人東海国立大学機構\ドキュメント\PZT圧電センサ\BPDatas_tmp" + '/*')
-path_list = glob.glob("/Users/inayoshikouya/Downloads/230614/*")
-print(f"てすっとだよ\n{path_list}")
-fValueList = []
-targetVarList = []
-#各データをFV（特徴量）に変換
-for path in path_list:
-    csv = pd.read_csv(path, encoding="shift-jis")
-    print("↓"+path)
-    fValue = fv.convertToFV(csv, plot=True, fileName=os.path.basename(path).split('.', 1)[0])
-    fValueList.append(fValue)
-    targetVarList.append([csv["SBP"][0], csv["DBP"][0]])
-    
-data = exportCSV(fValueList, targetVarList, "out0614.csv")
-
-#data = pd.read_csv("out4.csv")
-#deepL.deepL(data)
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+data = exportCSV("/home/k_inayoshi/DeepL_PiezoElectricSensor/BPDatas_tmp/*", "out0618.csv")
